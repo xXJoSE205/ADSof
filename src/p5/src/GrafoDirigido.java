4c6c3854bbb7f295ed42dfc122947aa3716f75fd
jrgmat98 <jorgematseg98@gmail.com>
2018-04-23 10:10:08 +0200

    Astolfo suck ur dick and call u gay

---
+++ src/p5/src/GrafoDirigido.java
@@ -0,0 +1,12 @@
+package p5.src;
+
+public class GrafoDirigido extends Grafo {
+
+    public GrafoDirigido() {
+
+    }
+
+    public void addArco(Vertice<T> v1, Vertice<T> v2, double peso){
+
+    }
+}
--- src/p5/src/Vertice.java
+++ src/p5/src/Vertice.java
@@ -1,14 +1,40 @@
 package p5.src;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Vertice<T> {
     private final int id;
     private T datos;
     private static int contador=1;
+    private static List<Integer> lista=new ArrayList<>();
 
     public Vertice( T datos) {
+        for(Integer i: lista){
+            if(i==contador){
+                contador++;
+            } else if(contador<i){
+                break;
+            }
+        }
         id=contador;
         contador++;
         this.datos=datos;
+    }
+
+    public Vertice(int id, T datos){
+        if (id<this.contador){
+            return;
+        }
+        for(Integer i: lista){
+            if(id==i){
+                return;
+            }
+        }
+        this.id=id;
+        lista.add(id);
+        Collections.sort(lista);
     }
 
     public int getId() {
